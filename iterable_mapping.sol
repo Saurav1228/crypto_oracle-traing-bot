dad4ba576a0d6ae125bfe54ded594637cae2aeb0
Saurav <negisaurav3010@gmail.com>
2022-09-02 00:06:47 +0530

    iterable mapping updated

--- iterable_mapping.sol
+++ iterable_mapping.sol
@@ -4,17 +4,41 @@ pragma solidity 0.8.13;
 contract iterablemapping {
     address[] public addressArray;
     mapping(address => bool) public whitelist;
-    uint private count = 0;
+    uint256 private count = 0;
+    address private owner;
 
-    function addAddress(address addr) external view returns (string memory) {
-        if(whitelist[addr] == true) {
-            return "ALready whitelisted";
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function addAddress(address addr)
+        public
+        onlyOwner
+        returns (string memory value)
+    {
+        if (whitelist[addr] == true) {
+            return "Error! Already whitelisted";
+        } else {
+            whitelist[addr] = true;
+            addressArray.push(addr);
+            count++;
+            return "Successfully WHhitelisted";
         }
     }
 
-    function getCount() public view returns (uint){
+    function getCount() public view returns (uint256) {
         return count;
     }
 
-
+    function isWhitelisted(address addr) public view returns (bool) {
+        if (whitelist[addr] == true) {
+            return true;
+        }
+        return false;
+    }
 }
