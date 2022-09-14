cb9b44ab653ddd2c0d04fc63eebf19821da5fc9b
Saurav <negisaurav3010@gmail.com>
2022-09-14 16:51:16 +0530

    14 sep 2

--- calldata.sol
+++ calldata.sol
@@ -1,17 +1,22 @@
 pragma solidity 0.5.11;
 
 contract Test {
-    
     string stringTest;
-    
-    function memoryTest(string memory _exampleString) public returns (string memory) {
-        _exampleString = "example";  // You can modify memory
-        string memory newString = _exampleString;  // You can use memory within a function's logic
-        return newString;  // You can return memory
+
+    function memoryTest(string memory _exampleString)
+        public
+        returns (string memory)
+    {
+        _exampleString = "example"; // You can modify memory
+        string memory newString = _exampleString; // You can use memory within a function's logic
+        return newString; // You can return memory
     }
-    
-    function calldataTest(string calldata _exampleString) external returns (string memory) {
-        // cannot modify _exampleString
+
+    function calldataTest(string calldata _exampleString)
+        external
+        returns (string memory)
+    {
+        // cannot modify _exampleString or use in any function's logic
         // but can return it
         return _exampleString;
     }
