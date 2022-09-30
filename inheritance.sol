2cbc00dc48ca39f173e1c4cc17587d1d5bf0d1a6
Saurav <negisaurav3010@gmail.com>
2022-09-30 19:14:47 +0530

    inheritance example

---
+++ inheritance.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract A {
+    function random() public pure virtual returns (string memory) {
+        return "A";
+    }
+}
+
+contract B is A {
+    function random() public pure virtual override returns (string memory) {
+        return "B";
+    }
+}
+
+contract C is B {
+    function random() public pure virtual override returns (string memory) {
+        return "C";
+    }
+}
+
+contract D is B, C {
+    function random() public pure override(C, B) returns (string memory) {
+        B.random();
+    }
+}
